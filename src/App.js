10f5a4906b3c9e54d1f0bca4b6e3ee1f07a97133
stamm2911 <edstamm29@gmail.com>
2021-09-23 00:45:17 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Dish from './components/Dish'
+
+function App() {
+  return (
+    <div className="App">
+      <Dish/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Dish.js
@@ -0,0 +1,17 @@
+function Dish() {
+  return (
+    <div className="dish-container">
+      <img src={"./images/Captura.jpg"} alt="pic" />
+      <aside>
+        <div className="dish-title-container">
+          <h2>Buttermilk Pancakes</h2>
+          <p>$69</p>
+        </div>
+        <hr/>
+        <p>I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed</p>
+      </aside>
+    </div>
+  );
+}
+
+export default Dish;
